7b6346e449390658893274895be9f1e2ae0926d8
신경은 <shingyeong-eun@singyeong-eun-ui-MacBook-Pro.local>
2017-03-13 16:47:05 +0900

    creating raw2annotation.py

---
+++ refine/raw2annotation.py
@@ -0,0 +1,9 @@
+import json
+import io
+
+with io.open('/Users/shingyeong-eun/Dropbox/논문/MSCOCO/captions_train-val2014/annotations/captions_val2014.json', 'r') as f:
+    dict1 = json.loads(f.read())
+    ex = dict1["annotations"]
+    with io.open('/Users/shingyeong-eun/Dropbox/논문/MSCOCO/captions_train-val2014/annotations/val_test.json', 'w') as t:
+        dump = json.dumps(ex)
+        t.write(dump)
