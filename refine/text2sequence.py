a1301fa8a3844e47b8351f3a677ee10b3ebc44f9
centurio1987 <ghkdldjwls@naver.com>
2017-03-13 18:04:37 +0900

    updating text2sequence.py v2

--- refine/text2sequence.py
+++ refine/text2sequence.py
@@ -14,7 +14,16 @@ with io.open('/Users/KYD/Dropbox/논문/MSCOCO/captions_train-val2014/annotation
 
     postagged_caption_list = []
     for elem in tokenized_caption_list:
-        postagged_caption_list.append(nltk.pos_tag(elem))
+        tagged = nltk.pos_tag(elem)
+        refined_tag = []
+        for item in tagged:
+            if item[1] == 'FW' or item[1] == 'JJ' or item[1] == 'NN'\
+                or item[1] == 'NNS' or item[1] == 'NNP' or item[1] == 'NNPS'\
+                or item[1] == 'VB' or item[1] == 'VBD' or item[1] == 'VBG'\
+                or item[1] == 'VBN' or item[1] == 'VBP' or item[1] == 'VBZ':
+                refined_tag.append(item)
+
+        postagged_caption_list.append(refined_tag)
 
     with io.open('/Users/KYD/Dropbox/논문/MSCOCO/captions_train-val2014/annotations/nltk_test.json', 'w') as ff:
         json_result = json.dumps(postagged_caption_list)
