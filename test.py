fc93a79ff4d6089497e5b99104195a15f296daa3
kyd <ghkdldjwls@naver.com>
2017-04-18 12:39:11 +0000

    2017/4/18

--- test.py
+++ test.py
@@ -6,14 +6,8 @@ from im2txt.im2txt.ops import image_processing
 from models import make_image_embeddings_cnn
 import numpy as np
 
-
-'''
-1. 이미지 불러오기
-2. 이미지 디코딩
-3. 스킵그램 페어
-'''
 #flags
-train_input_dir = "/home/ubuntu/data/images/1000_collected_train2014"
+test_input_dir = "/home/ubuntu/data/images/10_collected_train2014"
 metadata_file = '/home/ubuntu/data/metadata/img_metadata_train.json'
 word2id_file = '/home/ubuntu/data/metadata/word_to_index_dict.json'
 
@@ -24,7 +18,7 @@ def load_images(image_metadata_list):
     encoded_images = []
     extracted_metadata_by_exist = []
     for image_metadata in image_metadata_list:
-        filename = os.path.join(train_input_dir, image_metadata[1])
+        filename = os.path.join(test_input_dir, image_metadata[1])
         if tf.gfile.Exists(filename):
             with tf.gfile.FastGFile(filename, 'rb') as f:
                 encoded_images.append(f.read())
@@ -94,65 +88,32 @@ for image, metadata in image_and_metadata:
 #for i in labels:
 #    one_hot_labels.append(one_hot(len(word2id)+1, i))
 
-#X = tf.Variable(images)
-#Y = tf.Variable(labels)
-X = tf.placeholder(dtype=tf.float32, shape=[None, 299, 299, 3])
-Y = tf.placeholder(dtype=tf.int32, shape=[None, 1])
+#X = tf.placeholder(dtype=tf.float32, shape=[None, 299, 299, 3])
+#Y = tf.placeholder(dtype=tf.int32, shape=[None, 1, voc_size])
 
 w1 = tf.Variable(tf.random_normal([3, 3, 3, 32], stddev=0.01), name='w1')
 w2 = tf.Variable(tf.random_normal([3, 3, 32, 64], stddev=0.01), name='w2')
 w3 = tf.Variable(tf.random_normal([3, 3, 64, 128], stddev=0.01), name='w3')
 w4 = tf.Variable(tf.random_normal([128 * 38 * 38, 625], stddev=0.01), name='w4')
-p_keep_conv = 0.8
-p_keep_hidden = 0.5
+p_keep_conv = 1
+p_keep_hidden = 1
 
-#image_embedding
-image_embedding = make_image_embeddings_cnn.model(X, w1, w2, w3, w4, p_keep_conv, p_keep_hidden)
+image_embedding = make_image_embeddings_cnn.model(images, w1, w2, w3, w4, p_keep_conv, p_keep_hidden)
 
 w_o = tf.Variable(tf.random_normal(shape=[voc_size, image_embedding.shape.as_list()[1]]), name='output_weight')
 bias = tf.Variable(tf.zeros([voc_size]), name='bias')
 
-#train
-'''
-#using sampled_softmax_loss
-sampled_cost = tf.nn.sampled_softmax_loss(weights=w_o,
-                           biases=bias,
-                           labels=Y,
-                           inputs=image_embedding,
-                           num_sampled=14,
-                           num_classes=voc_size)
-
-cost = tf.reduce_sum(sampled_cost)
-'''
-#using nce_loss
-nce_cost = tf.nn.nce_loss(weights=w_o, biases=bias,
-                 inputs=image_embedding, labels=Y,
-                 num_sampled=14, num_classes=voc_size,
-                 name='nce_loss')
-cost = tf.reduce_sum(nce_cost)
-
-train_op = tf.train.RMSPropOptimizer(0.001, 0.9).minimize(cost)
-
-init = tf.global_variables_initializer()
+logits = tf.matmul(image_embedding, tf.transpose(w_o))
+logits = tf.nn.bias_add(logits, bias)
+labels_one_hot = tf.one_hot(labels, voc_size)
+soft_cost = tf.nn.softmax_cross_entropy_with_logits(
+      labels=labels_one_hot,
+      logits=logits)
+cost = tf.reduce_sum(soft_cost)
 
 saver = tf.train.Saver()
-#session
 with tf.Session() as sess:
-    sess.run(init)
-    epoch = 100
-    batch_size = 10
+    saver.restore(sess, 'saved_model/my-model.ckpt')
 
-    for current in range(epoch):
-        for start in range(0, len(skip_gram_pairs), batch_size):
-            end = start + batch_size
-            if end > len(skip_gram_pairs):
-                end = len(skip_gram_pairs)
-            batch_images = images[start:end]
-            batch_labels = labels[start:end]
-            x_data_list = []
-            for image in batch_images:
-                x_data_list.append(image.eval())
-
-            print('epoch %d, batch_start: %d, batch_end: %d cost: %f' % (current, start, end, sess.run(cost, feed_dict={X:x_data_list, Y:batch_labels})))
-            sess.run(train_op, feed_dict={X:x_data_list, Y:batch_labels})
-        saver.save(sess, 'saved_model/my-model.ckpt')
+    trained_image_embedding = image_embedding.eval()
+    
